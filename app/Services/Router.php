7c05efe278737c9d4fe57f95f53274df79f445ff
Vlad-Avoyan <avoyanvlad00@gmail.com>
2023-01-28 18:35:01 +0400

    first rout

--- app/Services/Router.php
+++ app/Services/Router.php
@@ -5,8 +5,24 @@ namespace App\Services;
 class Router
 {
 
-    public static function test() {
-        echo "123";
+    private static array $list = [];
+
+    public static function page($uri, $page_name) {
+
+        self::$list[] = [
+            "uri" => $uri,
+            "page" => $page_name
+        ];
+    }
+
+    public static function enable() {
+        $query = $_GET['q'];
+
+        foreach (self::$list as $route) {
+            if ($route['uri'] === '/' . $query) {
+                require_once "views/pages/login/" . $route['page'] . ".php";
+            }
+        }
     }
 
 }
