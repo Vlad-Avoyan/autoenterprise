fb11b510bb8cc263e0f9ce3dbd3eeca42f7bb0d1
Vlad-Avoyan <avoyanvlad00@gmail.com>
2023-01-28 19:18:11 +0400

    error page created

--- app/Services/Router.php
+++ app/Services/Router.php
@@ -21,8 +21,14 @@ class Router
         foreach (self::$list as $route) {
             if ($route['uri'] === '/' . $query) {
                 require_once "views/pages/login/" . $route['page'] . ".php";
+                die();
             }
+            self::not_found_page();
         }
+    }
+
+    private static function not_found_page() {
+        require_once "views/errors/404.php";
     }
 
 }
