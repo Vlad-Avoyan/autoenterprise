7fcfe88d29ef2078bb1428996998804e76f51939
Vlad-Avoyan <avoyanvlad00@gmail.com>
2023-01-29 18:30:33 +0400

    separation into components

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-</head>
-<body>
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-lg p-3 mb-5 bg-body rounded">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Autoroom</a>
@@ -26,11 +14,3 @@
         <a class="nav-link active" aria-current="page" href="#">login</a>
     </div>
 </nav>
-
-<div class="container">
-    <h3 class="mt-4">404: Page Not Found</h3>
-</div>
-
-
-</body>
-</html>
